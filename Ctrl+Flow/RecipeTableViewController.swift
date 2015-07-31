3ed0c57ebdce7ccb0e60bbd6f8ba589c60bf5ffa
Alec Snyder <linuxbash8@gmail.com>
2015-07-30 17:02:12 -0700

    Begin UI, setup resources

---
+++ Ctrl+Flow/RecipeTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeTableViewController.swift
+//  Ctrl+Flow
+//
+//  Created by Alec Snyder on 7/30/15.
+//  Copyright (c) 2015 Originate. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewController: UITableViewController {
+
+}
